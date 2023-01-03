82939556a9ef901978d1fd74955ff3c3bc6d9d7b
Bojana Bogicevic <bogicevicmbojana@gmail.com>
2023-01-03 17:17:38 +0100

    Add models setup and connection string

--- TaskManagementSystem/Shared/Models/Task.cs
+++ TaskManagementSystem/Shared/Models/Task.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer
+namespace Shared.Models
 {
-    public class Class1
+    class Task
     {
     }
 }
