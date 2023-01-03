cd7164f7620732036fbfd62d4193e2a487a54817
Bojana Bogicevic <bogicevicmbojana@gmail.com>
2023-01-03 20:24:57 +0100

    Add repo dependency injection

--- TaskManagementSystem/PresentationLayer/Program.cs
+++ TaskManagementSystem/PresentationLayer/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Extensions.DependencyInjection;
+using DataAccessLayer.Repositories;
 
 namespace PresentationLayer
 {
@@ -17,6 +19,14 @@ namespace PresentationLayer
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            services.AddScoped<IProjectManagerRepository, ProjectManagerRepository>();
+            services.AddScoped<ITeamMemberRepository, TeamMemberRepository>();
+            services.AddScoped<ITaskRepository, TaskRepository>();
         }
     }
 }
