a0d7f092ac2733cd22c37768164dbcf9d2111e80
Bojana Bogicevic <bogicevicmbojana@gmail.com>
2023-01-06 12:53:57 +0100

    Implement methods in TeamMemberBusiness

--- TaskManagementSystem/BusinessLayer/ITeamMemberBusiness.cs
+++ TaskManagementSystem/BusinessLayer/ITeamMemberBusiness.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Shared.Models;
 
 namespace BusinessLayer
 {
     public interface ITeamMemberBusiness
     {
+        List<Task> GetPersonalTasks(int teamMemberId);
+        string UpdateSelf(TeamMember tm);
+        TeamMember GetTeamMember(string email, string password);
     }
 }
--- TaskManagementSystem/BusinessLayer/TeamMemberBusiness.cs
+++ TaskManagementSystem/BusinessLayer/TeamMemberBusiness.cs
@@ -2,11 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Shared.Models;
+using DataAccessLayer.Repositories;
 
 namespace BusinessLayer
 {
     public class TeamMemberBusiness : ITeamMemberBusiness
     {
+        private readonly ITeamMemberRepository teamMemberRepository;
+
+        public TeamMemberBusiness(ITeamMemberRepository _teamMemberRepository)
+        {
+            teamMemberRepository = _teamMemberRepository;
+        }
+
+        public List<Task> GetPersonalTasks(int teamMemberId)
+        {
+            return this.teamMemberRepository.GetPersonalTasks(teamMemberId);
+        }
+
+        public string UpdateSelf(TeamMember tm)
+        {
+            int rowsAffected = this.teamMemberRepository.UpdateSelf(tm);
+
+            if(rowsAffected > 0)
+            {
+                return "Vaši podaci su uspešno ažurirani!";
+            }
+            else
+            {
+                return "Neuspešno ažuriranje Vaših podataka!";
+            }
+        }
+        public TeamMember GetTeamMember(string email, string password)
+        {
+            return this.teamMemberRepository.GetTeamMember(email, password);
+        }
     }
 }
