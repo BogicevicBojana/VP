bc1cb9b73a2a08b098351ce0eca917074281e4c5
Bojana Bogicevic <bogicevicmbojana@gmail.com>
2023-01-06 12:21:40 +0100

    Implement methods in AdminBusiness

--- TaskManagementSystem/BusinessLayer/AdminBusiness.cs
+++ TaskManagementSystem/BusinessLayer/AdminBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccessLayer.Repositories;
+using Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,61 @@ namespace BusinessLayer
 {
     public class AdminBusiness : IAdminBusiness
     {
+        private readonly IAdminRepository adminRepository;
+
+        public AdminBusiness(IAdminRepository _adminRepository)
+        {
+            adminRepository = _adminRepository;
+        }
+
+        public List<ProjectManager> GetAllProjectManagers()
+        {
+            return this.adminRepository.GetAllProjectManagers();
+        }
+        public string InsertProjectManager(ProjectManager pm)
+        {
+            int rowsAffected = this.adminRepository.InsertProjectManager(pm);
+
+            if (rowsAffected > 0)
+            {
+                return "Menadžer projekta je uspešno dodat!";
+            }
+            else
+            {
+                return "Neuspešno dodavanje menadžera projekta!";
+            }
+        }
+        public string UpdateProjectManager(ProjectManager pm)
+        {
+            int rowsAffected = this.adminRepository.UpdateProjectManager(pm);
+
+            if (rowsAffected > 0)
+            {
+                return "Menadžer projekta je uspešno ažuriran!";
+            }
+            else
+            {
+                return "Neuspešno ažuriranje menadžera projekta!";
+            }
+        }
+        public string DeleteProjectManager(int id)
+        {
+            int rowsAffected = this.adminRepository.DeleteProjectManager(id);
+
+            if (rowsAffected > 0)
+            {
+                return "Menadžer projekta je uspešno izbrisan!";
+            }
+            else
+            {
+                return "Neuspešno brisanje menadžera projekta";
+            }
+        }
+
+
+        public Admin GetAdmin(string email, string password)
+        {
+            return this.adminRepository.GetAdmin(email, password);
+        }
     }
 }
--- TaskManagementSystem/BusinessLayer/IAdminBusiness.cs
+++ TaskManagementSystem/BusinessLayer/IAdminBusiness.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace BusinessLayer
 {
     public interface IAdminBusiness
     {
+        List<ProjectManager> GetAllProjectManagers();
+        string InsertProjectManager(ProjectManager pm);
+        string UpdateProjectManager(ProjectManager pm);
+        string DeleteProjectManager(int id);
     }
 }
